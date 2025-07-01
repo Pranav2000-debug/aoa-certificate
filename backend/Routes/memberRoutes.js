a00ff4bd67053b8e3bb8ca1a848ad490907a57f9
Pranav2000-debug <pranav.ch410@gmail.com>
2025-07-02 00:32:43 +0530

    remove middleware for testing

--- backend/Routes/memberRoutes.js
+++ backend/Routes/memberRoutes.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const { addMember } = require("../Controllers/memberController");
-const { verifyApiKey } = require("../middleware/auth");
+// const { verifyApiKey } = require("../middleware/auth");
 const memberRoutes = express.Router();
 
-memberRoutes.post("/add-member", verifyApiKey, addMember);
+memberRoutes.post("/add-member", addMember);
 
 module.exports = memberRoutes;
